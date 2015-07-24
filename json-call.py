1098701e552db5b1e6f1925d90b7ee5323761085
fli136 <fli136@gmail.com>
2015-07-24 00:23:14 -0700

    added search by user input

--- json-call.py
+++ json-call.py
@@ -12,10 +12,15 @@ import requests
 #    print("failure")
 
 #make web api call w/ api key
-request_summoner = requests.get('https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/ILoveWho?api_key=149f0504-eace-4b70-abeb-99f0de4c2cdf')
+summonername = raw_input('Enter name: ')
+print summonername
+request_summoner = requests.get('https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/' + summonername + '?api_key=149f0504-eace-4b70-abeb-99f0de4c2cdf')
 
 #built-in json decoder loads JSON
 parsed_summoner = request_summoner.json()
 
 #print Summoner id
-print parsed_summoner["ilovewho"]["id"]
+sum_id = parsed_summoner[summonername]["id"]
+print sum_id
+request_matchh = requests.get('https://na.api.pvp.net/api/lol/na/v2.2/matchhistory/' + str(sum_id) + '?api_key=149f0504-eace-4b70-abeb-99f0de4c2cdf')
+#print request_matchh.json()
